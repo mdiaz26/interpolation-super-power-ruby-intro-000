8d5a3557c7e586f5c89263210017892390cc347d
Michael Diaz <mike.diaz006@gmail.com>
2019-12-14 17:47:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,17 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
-  new_array = []
-  index = 0
+  new_array = ["R: #{array[0]}, O: #{array[1]}"]
 
-    while index < array.length 
-      new_array <<
-      index += 1
-    end
   puts new_array
 end
+
+# def display_rainbow(array)
+#   new_array = []
+#   index = 0
+
+#     while index < array.length 
+#       new_array <<
+#       index += 1
+#     end
+#   puts new_array
+# end
